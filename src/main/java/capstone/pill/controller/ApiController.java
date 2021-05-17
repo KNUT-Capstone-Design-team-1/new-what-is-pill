311478ead6c5ec889ea43b37bde2316a32258555
treeanimal <seohanjin503@gmail.com>
2021-05-17 15:15:17 +0900

    abc

--- src/main/java/capstone/pill/controller/ApiController.java
+++ src/main/java/capstone/pill/controller/ApiController.java
@@ -4,6 +4,7 @@ import capstone.pill.dto.ApiRequestDto;
 import capstone.pill.dto.ApiResponseDto;
 import capstone.pill.service.RestService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,10 @@ public class ApiController {
 
     private final RestService restService;
 
+    @GetMapping("/")
+    public String home (){
+        return "홈 화면";
+    }
     @PostMapping("/search")
     public ApiResponseDto response(@RequestBody ApiRequestDto requestDto){
         ApiResponseDto res = restService.res(requestDto);
