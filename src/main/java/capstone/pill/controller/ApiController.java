708900628ed36687442daf8a4e9c3df2df0ee482
treeanimal <seohanjin503@gmail.com>
2021-05-23 22:22:19 +0900

    applicaton test

--- src/main/java/capstone/pill/controller/ApiController.java
+++ src/main/java/capstone/pill/controller/ApiController.java
@@ -5,13 +5,17 @@ import capstone.pill.dto.ApiResponseDto;
 import capstone.pill.service.RestService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Base64;
+
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ApiController {
 
     private final RestService restService;
@@ -34,8 +38,23 @@ public class ApiController {
     }
 
     @PostMapping("/application")
-    public String application(@RequestBody imageRequestDto imageRequestDto){
-        return "Success";
+    public applicationResponseTest application(@RequestBody imageRequestDto imageRequestDto){
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] image = decoder.decode(String.valueOf(imageRequestDto));
+
+        log.info("디코딩 전" + imageRequestDto.getImg_base64());
+        log.info("디코딩 후" + new String(image));
+
+        return new applicationResponseTest("잘~ 받았습니다. 당신도 잘 받으셨나요?");
+    }
+
+    @Data
+    static class applicationResponseTest{
+        private String message;
+
+        public applicationResponseTest(String message) {
+            this.message = message;
+        }
     }
 
 
