f4f5a121722b80444996a4cc997f1c93dbf20a41
YongSiHun <tlgns1537@naver.com>
2021-10-27 16:36:08 +0900

    stable version deploy

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bash /var/lib/jenkins/workspace/mh-login.sh
-docker build --tag 2mukee/wip-main /var/lib/jenkins/workspace/new-what-is-pill/main-server
-docker build --tag 2mukee/wip-dl /var/lib/jenkins/workspace/new-what-is-pill/deeplearning-server
-docker push 2mukee/wip-main
-docker push 2mukee/wip-dl
+docker build --tag 2mukee/wip-main-deploy:1.1 /var/lib/jenkins/workspace/new-what-is-pill/main-server
+docker build --tag 2mukee/wip-dl-deploy:1.1 /var/lib/jenkins/workspace/new-what-is-pill/deeplearning-server
+docker push 2mukee/wip-main-deploy:1.1
+docker push 2mukee/wip-dl-deploy:1.1
 docker stack deploy -c /var/lib/jenkins/workspace/new-what-is-pill/wip-compose.yml wip-stack
