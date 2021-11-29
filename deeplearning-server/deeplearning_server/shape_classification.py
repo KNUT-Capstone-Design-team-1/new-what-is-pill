86a157eeb7c9e2b8c3e69653bea1381f6ef951b2
Jtea <jintea519@gmail.com>
2021-11-30 02:06:52 +0900

    remove unnecessary files and build test

---
+++ deeplearning-server/deeplearning_server/shape_classification.py
@@ -0,0 +1,43 @@
+import torchvision.transforms as transforms
+from PIL import Image
+
+from efficientnet_pytorch import EfficientNet
+import torch
+import numpy as np
+
+def transform_image(image_data):
+    # 이미지 전처리
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.fromarray(image_data, 'RGB')
+    # image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
+    return my_transforms(image).unsqueeze(0)
+
+def detect_pill_shape(img_data):
+    model_name = 'efficientnet-b7'
+    # 신경망 초기화
+    model = EfficientNet.from_pretrained(model_name, num_classes=2)
+
+    #학습된 모델 불러오기
+    model.load_state_dict(torch.load('C:\\Users\\jinte\\Desktop\\what_is_pill_app\\new-what-is-pill\\deeplearning-server\\weights\\pill_shape_fine_tuned.pt', map_location='cpu'))
+    model.eval()
+
+    try:
+        # 이미지 전처리
+        inputs = transform_image(image_data=img_data)
+
+        # 알약 모양 예측
+        outputs = model(inputs)
+        # 결과를 사용가능하게 변환
+        _, predict = torch.max(outputs, 1)
+        if int(predict.numpy()) == 0:
+            pill_shape = "원형"
+        else:
+            pill_shape = "타원형"
+        return pill_shape
+    except:
+        print("shape_classification error")
