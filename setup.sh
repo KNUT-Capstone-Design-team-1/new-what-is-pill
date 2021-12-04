e236678e56635f62d6ba8a72ce341c379d853f57
YongSiHun <tlgns1537@naver.com>
2021-12-04 23:29:31 +0900

    deeplearning server docker image file modified

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bash /var/lib/jenkins/workspace/mh-login.sh
 docker build --tag 2mukee/wip-main-deploy:latest /var/lib/jenkins/workspace/new-what-is-pill/main-server
-docker build --tag 2mukee/wip-dl-deploy:1.1 /var/lib/jenkins/workspace/new-what-is-pill/deeplearning-server
+docker build --tag 2mukee/wip-dl /var/lib/jenkins/workspace/new-what-is-pill/deeplearning-server
 docker push 2mukee/wip-main-deploy:latest
-docker push 2mukee/wip-dl-deploy:1.1
+docker push 2mukee/wip-dl
 docker stack deploy -c /var/lib/jenkins/workspace/new-what-is-pill/wip-compose.yml wip-stack
